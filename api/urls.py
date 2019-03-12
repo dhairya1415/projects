4c9e69c0ec839a4f28b5511fc7eed7d8e058fa67
Vikrant Gajria <vikrantgajria@gmail.com>
2019-03-12 12:18:32 +0530

    Separate urls

--- api/urls.py
+++ api/urls.py
@@ -18,6 +18,15 @@ from django.urls import path, include
 
 from rest_framework import routers
 
+from . import views
+
+router = routers.DefaultRouter()
+router.register("event", views.EventViewSet)
+
 urlpatterns = [
-    path("", include('api.urls')),
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("signup/", views.SignUp.as_view(), name="signup"),
+    path("login/", views.Login.as_view(), name="login"),
+    path("logout/", views.Logout.as_view(), name="logout"),
 ]
