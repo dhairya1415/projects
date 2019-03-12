5555eeecdaa68008816aefe6fbf13659c8a4f810
preet4737 <preet4737@gmail.com>
2019-03-12 12:25:46 +0530

    Register Admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import User,Event,Image
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class UserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class UserAdmin(UserAdmin):
+    form = UserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Event)
+admin.site.register(Image)
