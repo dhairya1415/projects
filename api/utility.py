ff47a30079591f858ff450be3e29aa137a042430
parag vaid <you@example.com>
2019-03-17 23:06:54 +0530

    Integrate Pdf and CSV

--- api/utility.py
+++ api/utility.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import pandas as pd
+from pdf_generation.task import import_data
 
 
 def generate_csv(report_data, event_data):
@@ -10,3 +11,6 @@ def generate_csv(report_data, event_data):
     df = df.drop(range(1, len(report_data["image"])))
     yf = pf.join(df)
     yf.to_csv("media/csv/{}.csv".format(event_data["name"]))
+    file = "media/csv/{}.csv".format(event_data["name"])
+    print(file)
+    import_data(file)
--- pdf_generation/task.py
+++ pdf_generation/task.py
@@ -1,14 +1,12 @@
-from reportlab.pdfgen import  canvas
+from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter, landscape, portrait, A4
 from reportlab.platypus import Image
 from reportlab.lib.units import inch, cm
 import csv
 
 
-data_file = 'data.csv'
-
 def import_data(data_file):
-    attendee_data = csv.reader(open(data_file, 'r'))
+    attendee_data = csv.reader(open(data_file, "r"))
     for row in attendee_data:
         event_name = row[0]
         event_venue = row[1]
@@ -19,25 +17,52 @@ def import_data(data_file):
         event_description = row[5]
         event_date = row[6]
         pdf_file_name = event_name + "$" + event_date + ".pdf"
-        generate_pdf(event_name, event_venue, no_of_participants, event_department ,image, event_description, event_date, pdf_file_name)
+        generate_pdf(
+            event_name,
+            event_venue,
+            no_of_participants,
+            event_department,
+            image,
+            event_description,
+            event_date,
+            pdf_file_name,
+        )
 
-def generate_pdf(event_name, event_venue, no_of_participants, event_department, image, event_description, event_date, pdf_file_name):
-    c = canvas.Canvas(pdf_file_name, pagesize=portrait(A4))
+
+def generate_pdf(
+    event_name,
+    event_venue,
+    no_of_participants,
+    event_department,
+    image,
+    event_description,
+    event_date,
+    pdf_file_name,
+):
+    c = canvas.Canvas("media/pdf/{}".format(pdf_file_name), pagesize=portrait(A4))
     c.setFont("Helvetica-Bold", 30, leading=None)
-    c.drawCentredString(4.135*inch, 11.19*inch, "Report for the Event:")
+    c.drawCentredString(4.135 * inch, 11.19 * inch, "Report for the Event:")
     c.setFont("Helvetica", 20, leading=None)
-    c.drawCentredString(4.135*inch, 10.69*inch, event_name + " | " + event_date)
-    c.drawCentredString(4.135*inch, 10.50*inch, "-----------------------------------------------------------")
+    c.drawCentredString(4.135 * inch, 10.69 * inch, event_name + " | " + event_date)
+    c.drawCentredString(
+        4.135 * inch,
+        10.50 * inch,
+        "-----------------------------------------------------------",
+    )
     c.setFont("Helvetica", 15, leading=None)
-    c.drawString(0.4*inch, 9.69*inch, "Event Department: {}".format(event_department))
-    c.drawString(0.4*inch, 9.19*inch, "Event Venue: {}".format(event_venue))
-    c.drawString(0.4*inch, 8.69*inch, "No. of participants: {}".format(no_of_participants))
-    c.drawString(0.4*inch, 8.19*inch, "Event Description:")
+    c.drawString(
+        0.4 * inch, 9.69 * inch, "Event Department: {}".format(event_department)
+    )
+    c.drawString(0.4 * inch, 9.19 * inch, "Event Venue: {}".format(event_venue))
+    c.drawString(
+        0.4 * inch, 8.69 * inch, "No. of participants: {}".format(no_of_participants)
+    )
+    c.drawString(0.4 * inch, 8.19 * inch, "Event Description:")
     c.setFont("Helvetica", 13, leading=None)
-    c.drawString(0.4*inch, 7.9*inch, event_description)
+    c.drawString(0.4 * inch, 7.9 * inch, event_description)
     c.setFont("Helvetica", 15, leading=None)
-    c.drawString(6*inch, 2*inch, "Signature")
-    c.drawString(6.25*inch, 1.7*inch, "(xyz)")
+    c.drawString(6 * inch, 2 * inch, "Signature")
+    c.drawString(6.25 * inch, 1.7 * inch, "(xyz)")
 
     # textobject = c.beginText()
     # textobject.setTextOrigin(0.2*inch, 7.69*inch)
@@ -47,18 +72,14 @@ def generate_pdf(event_name, event_venue, no_of_participants, event_department, 
 
     c.showPage()
     c.setFont("Helvetica", 15, leading=None)
-    c.drawString(0.4*inch, 11.19*inch, "Images:")
+    c.drawString(0.4 * inch, 11.19 * inch, "Images:")
     v_gap = 8.19
     for item in image:
         if v_gap > 0:
-            c.drawImage(item, 0.4*inch, v_gap*inch, width=200, height=200)
+            c.drawImage(item, 0.4 * inch, v_gap * inch, width=200, height=200)
             v_gap -= 3
     c.showPage()
     c.save()
 
-if __name__=="__main__":
-    import_data(data_file)
 
 # A4 dimensions: 8.27 × 11.69 inches
-
-
