0a836edefbfa9961b4f6ec0b131e74391d4f3e69
Vikrant Gajria <vikrantgajria@gmail.com>
2019-03-21 12:35:09 +0530

    Add CORS

--- project-vp/settings.py
+++ project-vp/settings.py
@@ -49,6 +49,7 @@ EMAIL_USE_TLS = True
 
 INSTALLED_APPS = [
     "api",
+    'corsheaders',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -61,6 +62,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -69,6 +72,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+APPEND_SLASH = False
 
 ROOT_URLCONF = "project-vp.urls"
 
